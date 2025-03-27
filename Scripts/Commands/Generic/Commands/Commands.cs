4359880ed729bf41c488377012def7c020768e00
Tabain84 <ktnyholm@gmail.com>
2025-03-27 00:17:10 -0500

    Removed Teleport effect from Tele command.
    Fixed compile warnings.
    
    Stealth now combined into Hiding.
    
    Treasure Hunting now combines Cartography, Lockpicking, and Remove Traps into one Skill.

--- Scripts/Commands/Generic/Commands/Commands.cs
+++ Scripts/Commands/Generic/Commands/Commands.cs
@@ -553,19 +553,6 @@ namespace Server.Commands.Generic
 
             from.Location = toLoc;
             from.ProcessDelta();
-
-            if (!from.Hidden)
-            {
-                Effects.SendLocationParticles(
-                    EffectItem.Create(fromLoc, from.Map, EffectItem.DefaultDuration),
-                    0x3728,
-                    10,
-                    10,
-                    2023);
-                Effects.SendLocationParticles(EffectItem.Create(toLoc, from.Map, EffectItem.DefaultDuration), 0x3728, 10, 10, 5023);
-
-                from.PlaySound(0x1FE);
-            }
         }
     }
 
